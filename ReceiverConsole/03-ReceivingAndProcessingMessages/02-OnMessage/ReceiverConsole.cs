940ab6f7a109d0bc0e21c7fc36d8bbebd6e1b87b
Ashim <ashim.sp@gmail.com>
2017-11-15 13:42:05 +0530

    Create ReceiverConsole.cs

---
+++ ReceiverConsole/03-ReceivingAndProcessingMessages/02-OnMessage/ReceiverConsole.cs
@@ -0,0 +1,80 @@
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+using WorkingWithMessages.Config;
+using WorkingWithMessages.DataContracts;
+
+namespace WorkingWithMessages.Receiver
+{
+    class ReceiverConsole
+    {
+        static QueueClient m_QueueClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to receive.");
+            Console.ReadLine();
+
+            CreateQueue();
+            ReceiveAndProcessPizzaOrdersUsingOnMessage(1);
+			      ReceiveAndProcessPizzaOrdersUsingOnMessage(5);
+			      ReceiveAndProcessPizzaOrdersUsingOnMessage(100);
+            
+            Console.WriteLine("Receiving, hit enter to exit.");
+            Console.ReadLine();
+            StopReceiving();
+        }
+		
+		    private static void CreateQueue()
+        {
+            var manager = NamespaceManager.CreateFromConnectionString
+			          (Settings.ConnectionString);
+			
+            if (!manager.QueueExists(Settings.QueueName))
+            {
+                Console.WriteLine("Creating queue: " + Settings.QueueName + ".....");
+                manager.CreateQueue(Settings.QueueName);
+                Console.WriteLine("Done!");
+            }
+        }
+
+        private static void ReceiveAndProcessPizzaOrdersUsingOnMessage(int threads)
+        {
+            // Create a new client
+            m_QueueClient = QueueClient.CreateFromConnectionString
+                (Settings.ConnectionString, Settings.QueueName);
+
+            // Set the options for using OnMessage
+            var options = new OnMessageOptions()
+            {
+                AutoComplete = false,
+                MaxConcurrentCalls = threads,
+                AutoRenewTimeout = TimeSpan.FromSeconds(30)
+            };
+
+            // Create a message pump using OnMessage
+            m_QueueClient.OnMessage(message =>
+            {
+                // Deserialize the message body
+                var order = message.GetBody<PizzaOrder>();
+
+                // Process the message
+                CookPizza(order);
+
+                // Complete the message
+                message.Complete();
+            }, options);
+
+            Console.WriteLine("Receiving, hit enter to exit");
+            Console.ReadLine();            
+        }
+
+        private static void StopReceiving()
+        {
+            // Close the client, which will stop the message pump.
+            m_QueueClient.Close();            
+        }
+    }
+}
